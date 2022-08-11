b7eb4e22bc5360d38a6bfcfd491de9607fd489d5
swatikanishu <nishuswatika@gmail.com>
2022-08-11 10:14:01 +0530

    Added:view details using JSON Object

--- js/HomePage.js
+++ js/HomePage.js
@@ -4,25 +4,62 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHtml = () => {
     
-    const HeaderHtml = "<tr><th>Profile</th><th>Name</th><th>Gender</th><th>Department</th>"+
+    const headerHtml = "<tr><th>Profile</th><th>Name</th><th>Gender</th><th>Department</th>"+
     "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-    const innerHtml = `${HeaderHtml}
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
             <tr>
-                <td><img class="profile" alt="" src="/asset/profile_img-3.png">
+                <td><img class="profile" alt="" src="${empPayrollData._profilePic}">
                 </td>
-                <td>Swatika Singh</td>
-                <td>Female</td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHtml(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
                 <td>
-                    <div class='dept-label'>HR</div>
-                    <div class='dept-label'>Finance</div>
-                </td>
-                <td>4000000</td>
-                <td>8 August 2022</td>
-                <td>
+    
                     <img id="1" onclick="remove(this)" alt="delete" src="/asset/delete-black-18dp.svg">
                     <img id="1" alt="edit" onclick="update(this)" src="/asset/create-black-18dp.svg">
                 </td>
             </tr>
-            `;
+            `};
     document.querySelector('#display').innerHTML = innerHtml;
+}
+createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Swatika Singh',
+            _gender: 'Female',
+            _department: [
+                'Sales',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '1 July 2022',
+            _notes: 'Test1',
+            _profilePic: '/asset/profile_img-3.png'
+        },
+        {
+            _name: 'Saurabh Singh',
+            _gender: 'Male',
+            _department: [
+                'Sales'
+            ],
+            _salary: '300000',
+            _startDate: '10 November 2022',
+            _notes: 'Test1',
+            _profilePic: '/asset/profile_img-2.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
